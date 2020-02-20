98233efd83d6e1807ae20d422d3fbc02d99875b4
yufias <yupi41@gmail.com>
2020-02-20 21:01:30 +0700

    created template view for lobby room list

---
+++ src/components/RoomList.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-success" style="margin: 12px;">Create room</button>
+        <div class="room-container">
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+            <b-card
+                title="Room name"
+                style="max-width: 20rem;"
+                class="room-card"
+            >
+                <b-card-text>
+                    Room description goes here
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Join room</b-button>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'RoomList'
+}
+</script>
+
+<style>
+    .room-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        width: 100%;
+    }
+    .room-card {
+        margin: 12px;
+    }
+</style>
---
+++ src/views/Lobby.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <center><h2>Tap-tap Racer</h2></center>
+    <RoomList />
+  </div>
+</template>
+
+<script>
+import RoomList from '@/components/RoomList.vue'
+export default {
+  name: 'Lobby',
+  components: {
+    RoomList
+  }
+}
+</script>
+
+<style>
+</style>
