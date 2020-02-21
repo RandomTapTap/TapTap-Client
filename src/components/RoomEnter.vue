04fcc4df5a6ca4cb755ff08a0655b0b6154e9dfa
yufias <yupi41@gmail.com>
2020-02-21 08:52:45 +0700

    template in room update

--- src/components/RoomEnter.vue
+++ src/components/RoomEnter.vue
@@ -1,46 +1,70 @@
 <template>
     <div>
-        <div class="room-container">
-            <b-card
-                title="Player name"
-                img-src="https://picsum.photos/600/300/?image=25"
-                img-alt="Image"
-                img-top
-                style="max-width: 12rem;"
-                class="room-card"
-            >
-                <b-button href="#" variant="danger">Kick</b-button>
-            </b-card>
-            <b-card
-                title="Player name"
-                img-src="https://picsum.photos/600/300/?image=25"
-                img-alt="Image"
-                img-top
-                style="max-width: 12rem;"
-                class="room-card"
-            >
-                <b-button href="#" variant="danger">Kick</b-button>
-            </b-card>
-            <b-card
-                title="Player name"
-                img-src="https://picsum.photos/600/300/?image=25"
-                img-alt="Image"
-                img-top
-                style="max-width: 12rem;"
-                class="room-card"
-            >
-                <b-button href="#" variant="danger">Kick</b-button>
-            </b-card>
-            <b-card
-                title="Player name"
-                img-src="https://picsum.photos/600/300/?image=25"
-                img-alt="Image"
-                img-top
-                style="max-width: 12rem;"
-                class="room-card"
-            >
-                <b-button href="#" variant="danger">Kick</b-button>
-            </b-card>
+        <div class="main-container">
+            <div class="room-name">
+                <h2>Room name</h2>
+            </div>
+            <div class="room-master">
+                <b-card
+                    title="Room master"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    style="max-width: 12rem;"
+                    class="room-card"
+                >
+                    <b-card-text>
+                        player name
+                    </b-card-text>
+                </b-card>
+            </div>
+            <div class="player">
+                <div class="room-container">
+                    <b-card
+                        title="Player name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        style="max-width: 12rem;"
+                        class="room-card"
+                    >
+                        <b-button href="#" variant="danger">Kick</b-button>
+                    </b-card>
+                    <b-card
+                        title="Player name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        style="max-width: 12rem;"
+                        class="room-card"
+                    >
+                        <b-button href="#" variant="danger">Kick</b-button>
+                    </b-card>
+                    <b-card
+                        title="Player name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        style="max-width: 12rem;"
+                        class="room-card"
+                    >
+                        <b-button href="#" variant="danger">Kick</b-button>
+                    </b-card>
+                    <b-card
+                        title="Player name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        style="max-width: 12rem;"
+                        class="room-card"
+                    >
+                        <b-button href="#" variant="danger">Kick</b-button>
+                    </b-card>
+                </div>
+            </div>
+            <div class="start-container">
+                <b-button href="#" variant="success">Start</b-button>
+            </div>
         </div>
     </div>
 </template>
@@ -52,6 +76,12 @@ export default {
 </script>
 
 <style>
+    .main-container {
+        background-color: rgb(182, 177, 177);
+        border-radius: 8px;
+        margin: 14px;
+        padding: 8px;
+    }
     .room-container {
         display: flex;
         flex-direction: row;
@@ -60,5 +90,15 @@ export default {
     }
     .room-card {
         margin: 12px;
+    }
+    .room-master {
+        margin: 8px;
+    }
+    .start-container {
+        display: flex;
+        justify-content: center;
+    }
+    h2 {
+        color: black;
     }
 </style>
